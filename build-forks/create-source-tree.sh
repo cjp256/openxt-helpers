fb09c6c4192fb3b03af91fbd260aa48bceb5d9b0
Chris Patterson <pattersonc@ainfosec.com>
2016-12-07 15:49:22 -0500

    build-forks/create-source-tree.sh: do not rely on executing within same dir
    Signed-off-by: Chris Patterson <pattersonc@ainfosec.com>

--- build-forks/create-source-tree.sh
+++ build-forks/create-source-tree.sh
@@ -3,10 +3,11 @@
 set -e
 
 SOURCES_DIR="${HOME}/openxt/sources/$1"
+REPO_LIST_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/repo.list"
 
 mkdir -p "${SOURCES_DIR}"
 
-for x in $(cat repo.list); do
+for x in $(cat "${REPO_LIST_PATH}"); do
   git clone --mirror "git://github.com/openxt/${x}.git" "${SOURCES_DIR}/${x}.git"
   git clone "${SOURCES_DIR}/${x}.git" "${SOURCES_DIR}/${x}"
 done
