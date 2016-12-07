5a4f0484e74dc3830f90ca7f8d4b169ae4d80eb5
Chris Patterson <pattersonc@ainfosec.com>
2016-12-07 15:52:25 -0500

    build-forks/create-build-tree.sh: do not rely on executing within same dir
    Signed-off-by: Chris Patterson <pattersonc@ainfosec.com>

--- build-forks/create-build-tree.sh
+++ build-forks/create-build-tree.sh
@@ -3,6 +3,7 @@
 BUILD_DIR="${HOME}/openxt/builds/$1"
 SOURCES_DIR="${HOME}/openxt/sources/$1"
 CERTS_DIR="${HOME}/openxt/certs"
+CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/config"
 
 if [[ -e ${BUILD_DIR} ]]; then
 	echo "build dir exists... aborting."
@@ -11,7 +12,7 @@ fi
 
 git clone ${SOURCES_DIR}/openxt.git "${BUILD_DIR}"
 
-cp config "${BUILD_DIR}/.config"
+cp "${CONFIG_PATH}" "${BUILD_DIR}/.config"
 
 sed -i "s|__GIT_MIRROR__|${SOURCES_DIR}|g" "${BUILD_DIR}/.config"
 sed -i "s|__CERTS_DIR__|${CERTS_DIR}|g" "${BUILD_DIR}/.config"
